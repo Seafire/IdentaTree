448b143a2bea51fef94a12b0dab9cbda264b6d40
Shane Ellis <sa.ellis.88@hotmail.com>
2016-09-21 21:50:26 +0100

    yoyoyo

---
+++ RainbowJam/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,48 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BUTTON_SELECT = 1135171848U;
+        static const AkUniqueID CLOUDRAINDROP_ONPLAYER = 1747880148U;
+        static const AkUniqueID ENEMY_ONTREE = 3001919793U;
+        static const AkUniqueID ENEMY_SHOT_IMPACT = 1886070995U;
+        static const AkUniqueID MUSIC_LEVEL_01 = 1461357257U;
+        static const AkUniqueID PICKUP_COLLECT = 2191429798U;
+        static const AkUniqueID PICKUP_ONTREE = 3679524427U;
+        static const AkUniqueID POINTS_NEGATIVE = 3967362362U;
+        static const AkUniqueID POINTS_POSITIVE = 4013977002U;
+        static const AkUniqueID POWERUP_COLLECT = 1483123208U;
+        static const AkUniqueID POWERUP_SHOOT = 3277421527U;
+        static const AkUniqueID TREE_FULL = 1697344387U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ENEMY_SOUNDBANK = 1933438525U;
+        static const AkUniqueID MENU_SOUNDBANK = 1146882678U;
+        static const AkUniqueID MUSIC_LEVEL_01_SOUNDBANK = 680652643U;
+        static const AkUniqueID PICKUPS_SOUNDBANK = 1654811218U;
+        static const AkUniqueID UI_SOUNDBANK = 2454045173U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
